682212e98d0ea5eeb4062dc1e3bb9b5041e8ee48
Nick Pachulski <hello@nickpachulski.com>
2013-12-18 15:59:00 -0600

    New Bureau::Frame file added. Forgot to in last commit.

---
+++ app/bureau/bureau_frame.rb
@@ -0,0 +1,21 @@
+module Bureau
+  module Frame
+    def self.menu
+      screen = UIScreen.mainScreen.bounds.size
+      CGRectMake(0, StatusBarHeight, screen.width, screen.height-StatusBarHeight)
+    end
+
+    def self.for_state(state, sliding:width)
+      if state == :open
+        open(width)
+      else
+        UIScreen.mainScreen.bounds
+      end
+    end
+
+    def self.open(width)
+      screen = UIScreen.mainScreen.bounds.size
+      CGRectMake(width, 0, screen.width, screen.height)
+    end
+  end
+end
