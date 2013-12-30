ac0743aa89f733aa331a7e552dc4b9b6f2375291
Nick Pachulski <hello@nickpachulski.com>
2013-12-29 20:08:08 -0600

    Adding a file_dependency to compilation order.

--- lib/bureau.rb
+++ lib/bureau.rb
@@ -6,4 +6,7 @@ Motion::Project::App.setup do |app|
   Dir.glob(File.join(File.dirname(__FILE__), 'bureau/*.rb')).each do |file|
     app.files.unshift(file)
   end
+
+  app.files_dependencies 'lib/bureau/bureau.rb' => 'lib/bureau/bureau_menu.rb'
+  app.frameworks += ['QuartzCore']
 end
